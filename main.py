a399a3e01dfde3c1a0f4232f3760c146c27c1895
DeveloperTanmay <umas81261@gmail.com>
2025-02-11 18:50:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Odd Even
+InputFromUser = int(input("Enter A Number To Check : "))
+print("Number To Be Check : " ,InputFromUser)
+
+if InputFromUser%2==0 :
+    print("This is an Even Number")
+
+else :
+    print("This is an Odd Number")
